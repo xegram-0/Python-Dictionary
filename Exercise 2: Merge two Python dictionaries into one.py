445f43d021e3165bba48eb35abb5d65c4f5ab61d
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-05 21:35:56 +0700

    Explained the solution

--- Exercise 2: Merge two Python dictionaries into one.py
+++ Exercise 2: Merge two Python dictionaries into one.py
@@ -8,7 +8,11 @@
 
 def merge2dict(dict1:dict,dict2:dict):
     #print(dict1,dict2)
-    #print(dict1.update(dict2))
+
+    # dict1.update(dict2)
+    # print(dict1)
+    # update() method needs seperate line for it 
+    # before printing the result
     mergeDict:dict = dict1 | dict2
     print(mergeDict)
 
