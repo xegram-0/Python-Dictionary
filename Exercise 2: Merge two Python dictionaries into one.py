a6ab13adf17d094f002aa6b370aa13f389f80e05
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-05 21:34:00 +0700

    Solved 2

---
+++ Exercise 2: Merge two Python dictionaries into one.py
@@ -0,0 +1,20 @@
+# Exercise 2: Merge two Python dictionaries into one
+# dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+# dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+# Expected output:
+
+# {'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+def merge2dict(dict1:dict,dict2:dict):
+    #print(dict1,dict2)
+    #print(dict1.update(dict2))
+    mergeDict:dict = dict1 | dict2
+    print(mergeDict)
+
+def main():
+    dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+    dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+    merge2dict(dict1,dict2)
+if __name__=="__main__":
+    main()
