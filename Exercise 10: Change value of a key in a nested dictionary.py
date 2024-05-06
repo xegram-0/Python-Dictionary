33662f02ec610a79cbf1dca5c43ff5f8edf01561
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-06 16:11:11 +0700

    Solved 10

---
+++ Exercise 10: Change value of a key in a nested dictionary.py
@@ -0,0 +1,28 @@
+# Exercise 10: Change value of a key in a nested dictionary
+# Write a Python program to change Brad’s salary to 8500 in the following dictionary.
+
+# Given:
+
+# sample_dict = {
+#     'emp1': {'name': 'Jhon', 'salary': 7500},
+#     'emp2': {'name': 'Emma', 'salary': 8000},
+#     'emp3': {'name': 'Brad', 'salary': 500}
+# }
+
+# Expected output:
+
+# {
+#    'emp1': {'name': 'Jhon', 'salary': 7500},
+#    'emp2': {'name': 'Emma', 'salary': 8000},
+#    'emp3': {'name': 'Brad', 'salary': 8500}
+# }
+def main():
+    sample_dict = {
+        'emp1': {'name': 'Jhon', 'salary': 7500},
+        'emp2': {'name': 'Emma', 'salary': 8000},
+        'emp3': {'name': 'Brad', 'salary': 500}
+    }
+    sample_dict['emp3']['salary']=8500
+    print(sample_dict)
+if __name__=="__main__":
+    main()
